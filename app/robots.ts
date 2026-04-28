dc6cc4fb5398a711df681c4ab4cae89ad354e716
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-04-28 09:57:41 +0500

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+// app/robots.ts
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/private/'],
+      },
+    ],
+    sitemap: 'https://pakistanmodelshub.com/sitemap.xml',
+    host: 'https://pakistanmodelshub.com',
+  };
+}
