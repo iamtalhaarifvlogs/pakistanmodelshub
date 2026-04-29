42e0e8fbf64728bfdd4087111caf549427c51b30
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-04-29 06:23:06 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-white text-black">
-<WhatsappButton />
+<WhatsAppButton />
         {children}
       </body>
     </html>
